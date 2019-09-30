588351175bcbf9d5b38c29e343a950ce14292b3c
NParvedy <nicolasparvedy@gmail.com>
2019-09-30 02:26:24 +0200

    mise à jour

--- accueil/menu.php
+++ accueil/menu.php
@@ -1,5 +1,5 @@
 <ul id="menu_ul">
-  <li id="menu_arcade"><a href="#arcade"></a><svg viewBox="0 0 960 300"><symbol id="s-text">
+  <li id="menu_arcade"><a href="#arcade"><svg viewBox="0 0 960 300"><symbol id="s-text">
             <text id="arcade" text-anchor="middle" x="50%" y="40%">ARCADE
             </text>
         </symbol>
@@ -11,9 +11,10 @@
             <use xlink:href="#s-text" class="text-copy"></use>
         </g> 
     </svg>
+    </a>
     </li>
   <li id="menu_pseudo"><a href="../config/verification.php"><?php echo $_SESSION['pseudo']; ?></a></li>
- <li class="menu_spd"><a href="../score/scores.php">Scores<img id="score" src="../images-hub/trophe.png" width="55px" height="55px"></li></a>
+ <li class="menu_spd"><a href="../score/scores.php"><span>Scores</span><img id="score" src="../images-hub/trophe.png" width="55px" height="55px"></li></a>
   <li class="menu_spd"><a href="../settings/profil.php">Paramètres<img id="param" src="../images-hub/parametre.png" widht="55px" height="55px"></li></a>
   <li class="menu_spd"><a href="../accueil/deconnexion.php">Déconnexion<img id="deco" src="../images-hub/deconnexion.png" width="55px" height="55px"></li></a>
 
